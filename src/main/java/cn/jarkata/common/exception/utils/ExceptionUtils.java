4d1312affafb6e567cdd1142e32362c3e2613cf9
Jarkata <jarkata@foxmail.com>
2024-12-09 23:59:13 +0800

    1.0.1

--- src/main/java/cn/jarkata/common/exception/utils/ExceptionUtils.java
+++ src/main/java/cn/jarkata/common/exception/utils/ExceptionUtils.java
@@ -1,6 +1,11 @@
 package cn.jarkata.common.exception.utils;
 
+import cn.jarkata.common.exception.BusinessException;
+import cn.jarkata.common.exception.DBException;
+import cn.jarkata.common.exception.ResponseCode;
 import cn.jarkata.commons.utils.ReflectionUtils;
+
+import java.util.Objects;
 
 public class ExceptionUtils {
 
@@ -14,4 +19,33 @@ public class ExceptionUtils {
         return (String) getMessage;
     }
 
+    public static void throwBusiness(int result, ResponseCode responseCode) {
+        if (result <= 0) {
+            throw new BusinessException(responseCode);
+        }
+    }
+
+    public static void throwDbException(int result, ResponseCode responseCode) {
+        if (result <= 0) {
+            throw new DBException(responseCode);
+        }
+    }
+
+    public static void throwDbException(int result, ResponseCode responseCode, String message) {
+        if (result <= 0) {
+            throw new DBException(responseCode, message);
+        }
+    }
+
+    public static void requireNonNull(Object object, ResponseCode responseCode) {
+        if (Objects.isNull(object)) {
+            throw new BusinessException(responseCode);
+        }
+    }
+
+    public static void requireNonNull(Object object, ResponseCode responseCode, String message) {
+        if (Objects.isNull(object)) {
+            throw new BusinessException(responseCode, message);
+        }
+    }
 }

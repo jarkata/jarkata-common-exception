35756a1b7059961ff033f2b75064e0692636ee40
Jarkata <jarkata@foxmail.com>
2024-12-09 23:58:43 +0800

    1.0.1

--- src/main/java/cn/jarkata/common/exception/utils/ValidateUtils.java
+++ src/main/java/cn/jarkata/common/exception/utils/ValidateUtils.java
@@ -1,8 +1,23 @@
-package cn.jarkata.common.exception;
+package cn.jarkata.common.exception.utils;
+
+import cn.jarkata.common.exception.BusinessException;
+import cn.jarkata.common.exception.DBException;
+import cn.jarkata.common.exception.ResponseCode;
+import cn.jarkata.commons.utils.ReflectionUtils;
 
 import java.util.Objects;
 
 public class ValidateUtils {
+
+    public static String getErrorMsg(Throwable ex) {
+        Object getMessage = ReflectionUtils.invokeMethod(ex, "getMessage");
+        return (String) getMessage;
+    }
+
+    public static String getErrorCode(Throwable ex) {
+        Object getMessage = ReflectionUtils.invokeMethod(ex, "getCode");
+        return (String) getMessage;
+    }
 
     public static void throwBusiness(int result, ResponseCode responseCode) {
         if (result <= 0) {
